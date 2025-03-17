abd26043faf1bdb322de60500a24584cccafca62
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2025-03-17 12:44:32 -0300

    3 falta terminar, 4 error push back

--- repaso/rapaso3.cpp
+++ repaso/rapaso3.cpp
@@ -3,43 +3,45 @@
 #include <vector>
 using namespace std;
 
+const int arreglo=10;
+
 struct producto
 {
     string nombre;
-    string precio;
+    int precio;
 };
 
 struct producto_cantidad
 {
     producto product;
-    int cantidad;
+    float cantidad;
 };
 struct Pedido {
     int numero;
     string nombre;
     int precio;
-    vector<string> lista_productos;
+    vector<producto_cantidad> lista_productos;
 };
 
-void cancelar_pedido(vector<Pedido> &pedidos_pendientes,vector<producto> productos,int &pedido)
+void cancelar_pedido(vector<Pedido> &pedidos_pendientes,vector<producto> productos)
 {   
     int numero_pedido;
     int exito = 0;
     cout << "diga  el numero de pedido que desea canselar" << endl;
-    cin >> numero;
-    for (int i = 0; i < pedidos_pendientes.size; i++)
+    cin >> numero_pedido;
+    for (int i = 0; i < pedidos_pendientes.size(); i++)
     {
         if(numero_pedido==pedidos_pendientes[i].numero){
             pedidos_pendientes.erase(i);
             exito=1;
         }
     }
-    if{exito==1}{
-        cout << "el pedido a sido cancelado" << endl;
-    }
-    else{
-        cout << "no hay un pedido con ese numero en este momento" << endl;
-    }
+        if(exito==1){
+            cout << "el pedido a sido cancelado" << endl;
+        }
+        else{
+            cout << "no hay un pedido con ese numero en este momento" << endl;
+        }
 }
 
 
@@ -47,43 +49,44 @@ void cancelar_pedido(vector<Pedido> &pedidos_pendientes,vector<producto> product
 
 
 
-void agregar_pedido(vector<Pedido> &pedidos_pendientes,producto productos,int &pedido)
+void agregar_pedido(vector<Pedido> &pedidos_pendientes,producto productos[10],int &pedido)
 {
-    
-    int eleccion;
-    int cantidad;
-    int preciofinal=0
-    string nombre;
-    vector<product> lista;
-    cout << "diga nombre para el pedido"
-    cin >> nombre;
+    pedido++;
+    int eleccion=1;
+    int preciofinal=0;
+    Pedido eligiendo;
+    producto_cantidad can_p;
+    vector<producto_cantidad> lista;
+    cout << "diga nombre para el pedido" << endl;
+    cin >> eligiendo.nombre;
+    eligiendo.numero=pedido;
     while (eleccion!=0)
     {
-        cout << "elija un producto de esta lista por el numero a su izquierda, si no quiere mas escriba 0" << endl
-        for (int i = 0; i < 10; i++)
+        cout << "elija un producto de esta lista por el numero a su izquierda, si no quiere mas escriba 0" << endl;
+        for (int i = 0; i < arreglo; i++)
         {
-            cout << productos[i].nombre << ' ' << productos[i].precio << endl;
+            cout <<i+1 << ')' << productos[i].nombre << ' ' << productos[i].precio << endl;
         }
         cin >> eleccion;
+        can_p.product=productos[eleccion-1];
         cout << "ingrese la canntidad de ese producto que quiere" << endl;
-        cin >> cantidad;
-        lista.pushback({eleccion,cantidad})
+        cin >> can_p.cantidad;
+        eligiendo.lista_productos.pushback(can_p);
     }
     for (int i = 0; i < lista.size(); i++)
     {
-        preciofinal+= lista[i].precio*lista[i].cantidad
-    }
-    
-    pedidos_pendientes.pushback(pedido,nombre,preciofinal,lista);
+        preciofinal+= eligiendo.lista_productos[i].cantidad * eligiendo.lista_productos[i].product.precio;
+        eligiendo.precio=preciofinal;
+        pedidos_pendientes.pushback(eligiendo);
 }
-
+}
 
 
 int main(){
     
     int pedido=0;
     vector<Pedido> pedidos_pendientes; 
-    producto productos[10]=
+    producto productos[arreglo]={
     {"Hamburguesa", 8},
     {"Pizza Margherita", 12},
     {"Ensalada César", 7},
@@ -93,7 +96,8 @@ int main(){
     {"Pollo Asado", 9.90},
     {"Paella", 14},
     {"Sushi Roll",13},
-    {"Postre de Chocolate", 6};
+    {"Postre de Chocolate", 6}
+};
 
 
 
---
+++ repaso4.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct date{
+    int dia;
+    int mes;
+    int año;
+};
+
+struct paciente{
+    string nombre;
+    int edad;
+    int numero_historial;
+    date fecha_cita; 
+};
+
+void Registrar_paciente(vector<paciente> &pacientes,int &n_historial){
+
+    paciente persona;
+
+    cout << "ingrese nombre del paciente" << endl;
+    cin >> persona.nombre;
+    cout << "ingrese edad del paciente" << endl;
+    cin >> persona.edad;
+    cout << "ingrese dia para la cita " << endl;
+    cin >> persona.fecha_cita.dia;    
+    cout << "ingrese mes para la cita " << endl;
+    cin >> persona.fecha_cita.mes;  
+    cout << "ingrese año para la cita " << endl;
+    cin >> persona.fecha_cita.año;  
+    cout << "cliente registrado" << endl;
+    n_historial++;
+    persona.numero_historial=n_historial;
+    pacientes.push_back(persona);
+    
+
+}
+
+
+void cancelar_cita(vector<paciente> &pacientes){
+
+    int n_historial;
+    bool exito=false;
+    cout << "ingrese su numero de historial para cancelar la cita" << endl;
+    cin >> n_historial; 
+    for (int i = 0; i < pacientes.size(); i++)
+    {
+        if(n_historial==pacientes[i].numero_historial){
+            pacientes[i].fecha_cita={0,0,0};
+            exito=true;
+        }
+    }
+    if(exito){
+        cout << "la cita ha sido cancelada" << endl;
+    }
+    else{
+        cout << "la cita ha sido cancelada" << endl;
+    }
+
+}
+
+
+void mostrar_citas(vector<paciente> &pacientes){
+
+    date fecha;
+    cout << "ingrese una fecha para ver todos los pedidos de esa" << endl;
+    cout << "Dia:";
+    cin >> fecha.dia;
+    cout << endl << "Mes:";
+    cin >> fecha.mes;
+    cout << endl << "año:";
+    cin >> fecha.año;
+    cout << endl;
+    cout << "En la fecha " << fecha.dia << '/' << fecha.mes << '/' << fecha.mes << " los pacientes que tienen cita este dia son:" << endl;
+    for (int i = 0; i < pacientes.size(); i++)
+    {
+        if((pacientes[i].fecha_cita.dia==fecha.dia)and(pacientes[i].fecha_cita.mes==fecha.mes)and(pacientes[i].fecha_cita.año==fecha.año)){
+             cout << "Paciente "<< pacientes[i].nombre << " con "<< pacientes[i].edad << "años, tiene el numero de historial "  << pacientes[i].numero_historial <<  endl;
+        }
+    }
+    
+}
+
+
+void buscar_data(vector<paciente> &pacientes){
+    int n_historial;    
+    cout << "ingrese su numero de historial para cancelar la cita" << endl;
+    cin >> n_historial; 
+    for (int i = 0; i < pacientes.size(); i++)
+    {
+        if(n_historial==pacientes[i].numero_historial){
+             cout << "Paciente "<< pacientes[i].nombre << " con "<< pacientes[i].edad << "años, tiene el numero de historial "  << pacientes[i].numero_historial << " ,tiene cita e la fecha " << pacientes[i].fecha_cita.dia << '/' << pacientes[i].fecha_cita.mes << '/' << pacientes[i].fecha_cita.mes << endl;
+        }
+    }
+}
+
+
+int main(){
+    int n_historial=0;
+        while(true){
+            int opcion=0;
+            vector<paciente> pacientes;
+            cout << "elija una opcion, si quiere salir escriba 0" << endl;
+            cout << "1) Registrar y sacar cita"  << endl;
+            cout << "2) Cancelar cita " << endl;
+            cout << "3) Mostrar citas" << endl;
+            cout << "4) Buscar imformacion" << endl;
+            cin >> opcion;
+            for (int i = 0; i < pacientes.size(); i++)
+    {
+             cout << "Paciente "<< pacientes[i].nombre << " con "<< pacientes[i].edad << "años, tiene el numero de historial "  << pacientes[i].numero_historial << " ,tiene cita e la fecha " << pacientes[i].fecha_cita.dia << '/' << pacientes[i].fecha_cita.mes << '/' << pacientes[i].fecha_cita.mes << endl;
+            }
+            switch (opcion)
+            {
+                case 0 :
+                    return 0;
+                break;
+                case 1 :
+                    Registrar_paciente(pacientes,n_historial);
+                    break;
+                case 2 :
+                    cancelar_cita(pacientes);
+                    break;
+                case 3 :
+                    mostrar_citas(pacientes);
+                    break;
+                case 4 :
+                    buscar_data(pacientes);
+                    break;
+                default:
+                    break;
+                }
+            
+        }
+}
+
