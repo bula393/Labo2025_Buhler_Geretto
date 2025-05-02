7923591857fc7954459d2313860507c737f8a363
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2025-05-02 16:39:08 -0300

    curso santi: modulo bucles finished

--- Curso_Buhler/duke/choice/Customer.java
+++ Curso_Buhler/duke/choice/Customer.java
@@ -2,11 +2,26 @@ package duke.choice;
 
 public class Customer {
     private String name;
+    private String size;
     public Customer() {
         this.name = "pepe";
     }
+
+    public Customer(String name, String size) {
+        this.name = name;
+        this.size = size;
+    }
+
     public Customer(String name) {
         this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
     }
 
     public String getName() {
--- Curso_Buhler/duke/choice/ShopApp.java
+++ Curso_Buhler/duke/choice/ShopApp.java
@@ -7,10 +7,45 @@ public class ShopApp {
         Customer customer = new Customer("Pinky");
         Clothing item1 = new Clothing("Blue jacket",20.9,"M");
         Clothing item2 = new Clothing("Orange T-Shirt", 12.5,"s");
+        Clothing item3 = new Clothing("Green Scarf", 5, "S");
+        Clothing item4 = new Clothing("Blue T-Shirt", 10.5, "s");
+        Clothing[] ropas = new Clothing[5];
+        ropas[0] = item1;
+        ropas[1] = item2;
+        ropas[2] = item2;
+        ropas[3] = item3;
+        ropas[4] = item4;
+        int measurement = 3;
+        switch (measurement) {
+            case 1,2,3:
+                customer.setSize("S");
+                break;
+            case 4,5,6:
+                customer.setSize("M");
+                break;
+            case 7,8,9:
+                customer.setSize("L");
+                break;
+            default:
+                customer.setSize("X");
+                break;
+        }
+        System.out.println(customer.getSize());
         System.out.println("hola " + customer.getName());
-        item1.print();
-        item2.print();
-        total += ((item1.getPrice() + item2.getPrice()*2)*(1+tax));
+        //item1.print();
+        //item2.print();
+        //total += ((item1.getPrice() + item2.getPrice()*2)*(1+tax));
+        for (Clothing c : ropas) {
+
+            if (c.getSize().equalsIgnoreCase(customer.getSize())) {
+                c.print();
+                total = total + c.getPrice();
+                if (total >= 15) {
+                    break;
+                }
+            }
+        }
+        total *= tax+1;
         System.out.println("total: " + total);
     }
 }
