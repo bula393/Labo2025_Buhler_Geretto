c4909c504e682e66a9bb5328bde36ca364d3dd3e
Recamm <alumno26.buhler.santiago@ipm.edu.ar>
2025-09-10 09:25:34 -0300

    terminado

--- Trabajos_java/Clase-objetos/sistemaArcoFlecha/SistemaBares.java
+++ Trabajos_java/Clase-objetos/sistemaArcoFlecha/SistemaBares.java
@@ -1,5 +1,6 @@
 package sistemaArcoFlecha;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class SistemaBares {
@@ -7,7 +8,7 @@ public class SistemaBares {
     private HashSet<Diana> tiposDianas;
     private HashSet<Beneficio> beneficios;
 
-    public SistemaBares(HashSet<Bar> bares, HashSet<String> tiposDianas, HashSet<Beneficio> beneficios) {
+    public SistemaBares(HashSet<Bar> bares, HashSet<Diana> tiposDianas, HashSet<Beneficio> beneficios) {
         this.bares = bares;
         this.tiposDianas = tiposDianas;
         this.beneficios = beneficios;
@@ -21,11 +22,11 @@ public class SistemaBares {
         this.bares = bares;
     }
 
-    public HashSet<String> getTiposDianas() {
+    public HashSet<Diana> getTiposDianas() {
         return tiposDianas;
     }
 
-    public void setTiposDianas(HashSet<String> tiposDianas) {
+    public void setTiposDianas(HashSet<Diana> tiposDianas) {
         this.tiposDianas = tiposDianas;
     }
 
@@ -46,12 +47,26 @@ public class SistemaBares {
         return masPuntajes;
     }
     public static void main(String[] args) {
-        DianaCircular dci = new DianaCircular();
-        DianaCuadrada dcu = new DianaCuadrada();
-        BeneficioTrago t1 = new BeneficioTrago();
-        BeneficioComida c1 = new BeneficioComida();
-        Bar b1 = new Bar();
-
+        DianaCircular dci = new DianaCircular("dianaredonda",new HashMap<>(),NivelDificultad.DIFICIL);
+        DianaCuadrada dcu = new DianaCuadrada("dianaCuadrada",new HashMap<>(),NivelDificultad.MEDIO,25.2);
+        BeneficioTrago t1 = new BeneficioTrago("descuentoFernet","franco",new HashSet<>());
+        BeneficioComida c1 = new BeneficioComida("2x1Hamburguesas","sanga",new HashSet<>());
+        HashSet<Diana> dianas = new HashSet<>();
+        dianas.add(dci);
+        dianas.add(dcu);
+        Bar b1 = new Bar(dianas,new HashSet<>(),new HashSet<>(),20.45,10);
+        b1.addBeneficio(t1);
+        b1.addBeneficio(c1);
+        b1.cantBeneficios();
+        HashSet<Bar> bares = new HashSet<>();
+        bares.add(b1);
+        HashSet<Beneficio> beneficios = new HashSet<>();
+        beneficios.add(t1);
+        beneficios.add(c1);
+        SistemaBares s1 = new SistemaBares(bares,dianas,beneficios);
+        System.out.println(s1.masPuntajes().getNombre());
+        dci.areaDiana();
+        dci.cantPuntajes();
     }
 
 }
